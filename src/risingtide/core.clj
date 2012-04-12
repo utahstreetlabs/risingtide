fa378ce7c6a87116a749c29903d2bcd20e395447
Travis Vachon <travis@copious.com>
2012-04-12 15:08:31 -0400

    Flush stdout after printing exceptions
    We're seeing delayed stacktraces in prod, so manually flush.

--- src/risingtide/core.clj
+++ src/risingtide/core.clj
@@ -13,9 +13,10 @@
 
 (defn safe-print-stack-trace
   [throwable]
-  (try
-    (.printStackTrace throwable (java.io.PrintWriter. *out*))
-    (catch Throwable t (prn "failed to print stack trace with error" t))))
+  (try (let [w (java.io.PrintWriter. *out*)]
+         (.printStackTrace throwable w)
+         (.flush w))
+    (catch Throwable t (log/error "failed to print stack trace with error" t))))
 
 (defmacro bench
   [msg & forms]
@@ -24,23 +25,3 @@
          result# (do ~@forms)]
      (log/info ~msg "in" (- (.getTime (java.util.Date.)) start#) "ms")
      result#))
-
-(comment
-  (redis/with-connection (redis/connection-map {}) (redis/rpush "resque:queue:stories" "{\"class\":\"Stories::AddInterestInActor\",\"args\":[47,634],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-aef04660348f5f018d1f\"}}"))
-
-(redis/with-connection (redis/connection-map {}) (redis/rpush "resque:queue:stories" "{\"class\":\"Stories::RemoveInterestInActor\",\"args\":[47,634],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-aef04660348f5f018d1f\"}}"))
-  (redis/with-connection (redis/connection-map {}) (redis/lpop "resque:queue:stories"))
-
-
-(dotimes [n 10000]  (redis/with-connection (redis/connection-map {}) (redis/rpush "resque:queue:rising_tide_stories" (str "{\"class\":\"Stories::Create\",\"args\":[{\"listing_id\":799,\"tag_ids\":[1],\"type\":\"listing_activated\",\"actor_id\":" 1 "}],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-3114fc3c8086dccb505e\"}}"))))
-
-(redis/with-connection (redis/connection-map {}) (redis/rpush "resque:queue:stories" (str "{\"class\":\"Stories::Create\",\"args\":[{\"listing_id\":799,\"tag_ids\":[1],\"type\":\"listing_activated\",\"actor_id\":" 1 "}],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-3114fc3c8086dccb505e\"}}")))
-
-(redis/with-connection (redis/connection-map {}) (redis/rpush "resque:queue:rising_tide_stories" (str "{\"class\":\"Stories::Create\",\"args\":[{\"listing_id\":799,\"tag_ids\":[1],\"type\":\"listing_activated\",\"actor_id\":" 48215 "}],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-3114fc3c8086dccb505e\"}}")))
-
-(redis/with-connection (redis/connection-map {}) (redis/rpush "resque:queue:stories" (str "{\"class\":\"Stories::Create\",\"args\":[{\"type\":\"user_joined\",\"actor_id\":47}],\"context\":{\"log_weasel_id\":\"BROOKLYN-RESQUE-52023d69d3bda45328d5\"}}")))
-
-
-  "{\"class\":\"Stories::AddInterestsInListing\",\"args\":[799],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-3114fc3c8086dccb505e\"}}"
-
-  "{\"class\":\"Stories::AddInterestInListing\",\"args\":[47,655],\"context\":{\"log_weasel_id\":\"BROOKLYN-WEB-4124458a124264522a02\"}}")

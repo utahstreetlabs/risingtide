eb6b722d95369cdcb9e479b56fd94932e5bde80d
Travis Vachon <travis@copious.com>
2012-04-19 14:22:18 -0400

    Bump accession for response parsing fix

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
                  [ring/ring-core "1.0.2"]
                  [ring/ring-jetty-adapter "1.0.2"]
                  [robert/bruce "0.7.1"]
-                 [utahstreetlabs/accession "0.1.2-usl3" :exclusions [org.clojure/clojure]]
+                 [utahstreetlabs/accession "0.1.2-usl4" :exclusions [org.clojure/clojure]]
 
                  [mycroft/mycroft "0.0.2"]]
 

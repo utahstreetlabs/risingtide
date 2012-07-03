998912901476278f2817d3881e783aa8e7e1bc72
Travis Vachon <travis@copious.com>
2012-07-03 12:37:24 -0400

    Upgrade RT to lein 2

--- project.clj
+++ project.clj
@@ -8,18 +8,18 @@
 
                  [clj-time "0.3.4"]
                  [clj-logging-config "1.9.6"]
+                 [compojure "1.1.0"]
                  [enlive "1.0.0"]
+                 [mycroft/mycroft "0.0.2"]
+                 [redis.clients/jedis "2.0.0"]
                  [ring/ring-core "1.0.2"]
                  [ring/ring-jetty-adapter "1.0.2"]
-                 [robert/bruce "0.7.1"]
-                 [redis.clients/jedis "2.0.0"]
+                 [robert/bruce "0.7.1"]]
 
-                 [mycroft/mycroft "0.0.2"]]
-
-  :dev-dependencies [[midje "1.3.1"]
-                     [lein-midje "1.0.8"]
-                     [lein-swank "1.4.4"]]
-  :repositories {"usl-releases" "s3p://utahstreetlabs-maven/releases/"
-                 "usl-snapshots" "s3p://utahstreetlabs-maven/snapshots/"}
+  :run-aliases {:convert-redis-keys-from-staging-to-dev! risingtide.utils/run-convert-redis-keys-from-staging-to-dev!}
   :main risingtide
-  :run-aliases {:convert-redis-keys-from-staging-to-dev! risingtide.utils/run-convert-redis-keys-from-staging-to-dev!})
+  :min-lein-version "2.0.0"
+  :plugins [[midje "1.3.1"]
+            [lein-midje "1.0.8"]
+            [lein-swank "1.4.4"]
+            [lein-ring "0.7.1"]])

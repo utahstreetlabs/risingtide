615c6fbaf86050d270ee19052f9091860857e1e7
Travis Vachon <travis@copious.com>
2012-11-01 19:17:35 -0400

    Make active user value a hash, store shard key in it
    Keep backwards compatibility ftw.

--- client/lib/rising_tide/models/active_users.rb
+++ client/lib/rising_tide/models/active_users.rb
@@ -4,20 +4,41 @@ module RisingTide
   class ActiveUsers < RedisModel
     include Ladon::Logging
 
-    class << self
-      attr_accessor :active_user_ttl
+    SHARD_KEY = "sc"
+
+    def self.active_user_ttl
+      Config.active_user_ttl || (7 * 24 * 60 * 60)
     end
 
-    def self.add_active(user_id)
+    def self.default_shard
+      Config.default_shard || 1
+    end
+
+    def self.set_active!(user_id)
       with_redis do |redis|
         key = active_user_key(user_id)
-        redis.set(key)
-        redis.expire(key, active_user_ttl)
+        unless redis.expire(key, active_user_ttl)
+          redis.hset(key, "sc", default_shard)
+          redis.expire(key, active_user_ttl)
+        end
       end
     end
 
     def self.active_user_key(user_id)
       format_key(:act, user_id)
+    end
+
+    def self.shard(user_id)
+      key = active_user_key(user_id)
+      with_redis do |redis|
+        redis.hget(key, SHARD_KEY) || '1'
+      end
+    end
+
+    class Config
+      class << self
+        attr_accessor :active_user_ttl, :default_shard
+      end
     end
   end
 end

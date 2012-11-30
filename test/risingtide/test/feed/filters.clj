5c354fef6f7b56c938f8e5a7128911d07fcfdec8
Travis Vachon <travis@copious.com>
2012-11-30 11:53:15 -0500

    Clean up actor blacklist a little
    Also use a defonce for env to avoid clobbering the alter-var-root we
    do in risingtide.test and re-setting the env to development.

--- test/risingtide/test/feed/filters.clj
+++ test/risingtide/test/feed/filters.clj
@@ -4,20 +4,28 @@
    [midje.sweet :refer :all]
    [risingtide.feed.filters :refer :all]))
 
-(facts
-  (for-user-feed? {}) => :ylf
-  (for-user-feed? {:feed [:ev]}) => nil
-  (for-user-feed? {:feed nil}) => true
-  (for-user-feed? {:feed [:ylf]}) => :ylf
-  (for-user-feed? {:feed ["ylf"]}) => :ylf
-  (for-user-feed? {:feed [:ylf :ev]}) => :ylf
-  (for-user-feed? {:feed ["ylf" "ev"]}) => :ylf
+(facts "about for-user-feed?"
+  (for-user-feed? {}) => truthy
+  (for-user-feed? {:feed [:ev]}) => falsey
+  (for-user-feed? {:feed nil}) => truthy
+  (for-user-feed? {:feed [:ylf]}) => truthy
+  (for-user-feed? {:feed ["ylf"]}) => truthy
+  (for-user-feed? {:feed [:ylf :ev]}) => truthy
+  (for-user-feed? {:feed ["ylf" "ev"]}) => truthy)
 
-  (for-everything-feed? {}) => :ev
-  (for-everything-feed? {:feed nil}) => true
-  (for-everything-feed? {:feed ["ylf"]}) => nil
-  (for-everything-feed? {:feed [:ylf]}) => nil
-  (for-everything-feed? {:feed ["ev"]}) => :ev
-  (for-everything-feed? {:feed [:ev]}) => :ev
-  (for-everything-feed? {:feed [:ylf :ev]}) => :ev
-  (for-everything-feed? {:feed ["ylf" "ev"]}) => :ev)
+(facts "about the user feed actor blacklist"
+  (binding [risingtide.config/*user-feed-actor-blacklist* {:test #{4}}]
+    (for-user-feed? {}) => truthy
+    (for-user-feed? {:actor-id 2 :feed [:ylf]}) => truthy
+    (for-user-feed? {:actor-id 4 :feed [:ylf]}) => falsey
+    (for-user-feed? {:feed [:ev]}) => falsey))
+
+(facts "about for-everything-feed?"
+  (for-everything-feed? {}) => truthy
+  (for-everything-feed? {:feed nil}) => truthy
+  (for-everything-feed? {:feed ["ylf"]}) => falsey
+  (for-everything-feed? {:feed [:ylf]}) => falsey
+  (for-everything-feed? {:feed ["ev"]}) => truthy
+  (for-everything-feed? {:feed [:ev]}) => truthy
+  (for-everything-feed? {:feed [:ylf :ev]}) => truthy
+  (for-everything-feed? {:feed ["ylf" "ev"]}) => truthy)
